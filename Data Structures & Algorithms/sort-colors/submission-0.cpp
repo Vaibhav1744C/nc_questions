57bbaddf03a7b128284379a1f541e6924c83458b
Vaibhav Nagargoje <nagargojevaibhav045@gmail.com>
2026-07-17 00:56:41 +0530

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int zero=0;
+        int one=0;
+        int two=0;
+
+        for(int num:nums){
+            if(num==0){
+                zero++;
+            }else if(num==1){
+                one++;
+            }else{
+                two++;
+            }
+
+        }
+        int i=0;
+        while(zero--){
+            nums[i++]=0;
+        }
+        while(one--){
+            nums[i++]=1;
+        }
+        while(two--){
+            nums[i++]=2;
+        }
+        
+    }
+};
